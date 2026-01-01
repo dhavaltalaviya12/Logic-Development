5f8136d0e5d4077184864404734c0ea04ca6a1cf
Dhaval Talaviya <146553819+dhavaltalaviya12@users.noreply.github.com>
2026-01-01 19:17:24 +0530

    Refactor conditionals in Q16.java for clarity

--- Q16.java
+++ Q16.java
@@ -2,7 +2,7 @@ public class Q16 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
-                if ((i <= 5 && j == 1 && j == 5) || (j == 9 && i >= 5) || (i <= 5 && j == 1) || (i == 5 || j == 5)
+                if ((j == 9 && i >= 5) || (i <= 5 && j == 1) || (i == 5 || j == 5)
                         || (i == 1 && j > 5) || (i == 9 && j <= 5)) {
                     System.out.print("*");
                 } else {
@@ -13,3 +13,4 @@ public class Q16 {
         }
     }
 }
+
